ba2066454fff42f0ff8153978b0d01ab53c9a0b0
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-04-22 11:02:05 -0400

    Add test.bat to run a variety of word ladder commands

---
+++ TEST.bat
@@ -0,0 +1,20 @@
+WordLadder OWL.txt clash clown
+WordLadder SOWPODS.txt clash clown
+WordLadder OWL.txt clash zzzzz
+WordLadder OWL.txt warm cold
+WordLadder SOWPODS.txt warm cold
+WordLadder OWL.txt ape man
+WordLadder SOWPODS.txt ape man
+WordLadder OWL.txt charge comedo
+WordLadder SOWPODS.txt charge comedo
+WordLadder OWL.txt cat dog
+WordLadder SOWPODS.txt cat dog
+WordLadder OWL.txt fool sage
+WordLadder SOWPODS.txt fool sage
+WordLadder
+WordLadder OWL.txt fool
+WordLadder OWL.txt
+WordLadder NonExistent.txt fool sage
+WordLadder OWL.txt zzzzz clash
+WordLadder OWL.txt atlases cabaret
+WordLadder SOWPODS.txt atlases cabaret
