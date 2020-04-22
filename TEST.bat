0d8215bc7cee6e028c94cef64b1e0acd4bc73210
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-04-22 11:10:09 -0400

    Update Test file

--- TEST.bat
+++ TEST.bat
@@ -1,6 +1,17 @@
+ECHO Word Ladder Testing
+ECHO Invalid commands
+WordLadder
+WordLadder OWL.txt fool
+WordLadder OWL.txt
+WordLadder NonExistent.txt fool sage
+
+ECHO Non-existent word ladders
+WordLadder OWL.txt zzzzz clash
+WordLadder OWL.txt clash zzzzz
+
+ECHO Some working ladders
 WordLadder OWL.txt clash clown
 WordLadder SOWPODS.txt clash clown
-WordLadder OWL.txt clash zzzzz
 WordLadder OWL.txt warm cold
 WordLadder SOWPODS.txt warm cold
 WordLadder OWL.txt ape man
@@ -11,10 +22,7 @@ WordLadder OWL.txt cat dog
 WordLadder SOWPODS.txt cat dog
 WordLadder OWL.txt fool sage
 WordLadder SOWPODS.txt fool sage
-WordLadder
-WordLadder OWL.txt fool
-WordLadder OWL.txt
-WordLadder NonExistent.txt fool sage
-WordLadder OWL.txt zzzzz clash
 WordLadder OWL.txt atlases cabaret
 WordLadder SOWPODS.txt atlases cabaret
+
+pause
