b640fed838f34fd40021b95f1f6c41d22105f948
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-04-24 15:18:09 -0400

    Slight updates to main

--- WordLadder/Main.cpp
+++ WordLadder/Main.cpp
@@ -2,7 +2,7 @@
 	Author: Quinn Kleinfelter
 	Class: EECS 2510-001 Non Linear Data Structures Spring 2020
 	Instructor: Dr. Thomas
-	Last Edited: 4/23/20
+	Last Edited: 4/24/20
 
 	Main File used to determine the shortest word ladder
 	from a user entered start word to a user entered 
@@ -28,8 +28,10 @@ int main(int argc, char* argv[])
 	{
 		// If we don't have exactly 4 arguments error out
 		cout << "Incorrect number of arguments" << endl;
+		cout << "Correct usage: WordLadder.exe lexiconFileName startingWord endingWord" << endl;
 		return 0;
 	}
+
 	string lexicon = argv[1]; // File location to use as our lexicon
 	string firstWord = argv[2]; // Starting word for the ladder
 	string lastWord = argv[3]; // Ending word for the ladder
@@ -42,7 +44,7 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 
-	WordLadder* wl = new WordLadder(lexicon, firstWord.length()); // Initialize our word ladder variable
+	WordLadder* wl = new WordLadder(lexicon, firstWord.length()); // Initialize our word ladder
 	toUpper(firstWord); // Convert the first word to upper case in place
 	toUpper(lastWord);  // Convert the last word to upper case in place
 	vector<string> minLadder = wl->getMinLadder(firstWord, lastWord); // Get the minimum word ladder from the first word to the last word
